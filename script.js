b6ec7e4edeb6c6a24354ee913829309dc23e0d36
gabrys-ei19 <102586104+gabrys-ei19@users.noreply.github.com>
2023-03-09 13:10:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-import pobranyJson from "http://imiki.pl/cf/" assert { type: "json" };
+import dane from "http://imiki.pl/cf/" assert { type: "json" };
 
-console.log(pobranyJson.data[0].state.temperature);
+document.write(dane.data[0].state.temperature);
